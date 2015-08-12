e5673c89f3865f421e8c27a6ef335c96b4665b1e
Cristian Prieto <me@cprieto.com>
2015-08-12 20:54:08 +1000

    Let was turning me crazy, so moved to var, in function scope both are the same

--- watcher.js
+++ watcher.js
@@ -1,15 +1,15 @@
 'use strict';
 
-const fs = require('fs'),
-    spawn = require('child_process').spawn,
-    filename = process.argv[2];
+const fs       = require('fs'),
+      spawn    = require('child_process').spawn,
+      filename = process.argv[2];
 
 if (!filename) {
     throw Error("You need to pass the name of the file to watch as parameter!");
 }
 
 fs.watchFile(filename, function () {
-    let ls = spawn('ls', ['-lh', filename]);
+    var ls = spawn('ls', ['-lh', filename]);
     ls.stdout.pipe(process.stdout);
 });
 
