bfd208c5fda25a142532d108a6eb234c0f7ed5a1
Cristian Prieto <me@cprieto.com>
2015-08-15 23:12:10 +1000

    Now I have code that show lack of parallelism

---
+++ chapter04/zmq-filer-req-loop.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const zmq      = require('zmq'),
+      req      = zmq.socket('req'),
+      filename = process.argv[2];
+
+if (!filename) {
+    throw new Error("You need to pass a filename as parameter.");
+}
+
+req.on('message', function (data) {
+    var response = JSON.parse(data);
+    console.log("Receiving response: " + response);
+});
+
+req.connect('tcp://localhost:5433');
+console.log("Sending request for '" + filename + "'.");
+
+for (let i = 0; i < 3; i++) {
+    console.log("Sending request " + i);
+    req.send(JSON.stringify({
+        path: filename
+    }));
+}
