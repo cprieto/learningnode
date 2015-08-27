7fd60050439ef9cb6d83ddaff561228d2550b139
Cristian Prieto <me@cprieto.com>
2015-08-27 18:29:14 +1000

    We have a simple command line stuff to test couchdb

--- chapter05/dbcli.js
+++ chapter05/dbcli.js
@@ -1,3 +1,14 @@
-/**
- * Created by cprieto on 27/08/15.
- */
+#!/usr/bin/env node --harmony
+'use strict';
+const request = require('request'),
+      options = {
+          method: process.argv[2] || 'GET',
+          url: 'http://localhost:5984/' + (process.argv[3] || '')
+      };
+request(options, function(err, res, body) {
+    if (err) {
+        throw Error(err);
+    } else {
+        console.log(res.statusCode, JSON.parse(body));
+    }
+});
